6839296725cc5ddf8278a783522615fd66d9ee53
Ken Ma <kenmashikin@gmail.com>
2024-04-09 18:00:39 +0800

    feat: check files before create
    feat: dummy-proof the new test script

--- newTest.sh
+++ newTest.sh
@@ -1,3 +1,19 @@
-cp template.html "./tests/$1.html"
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+    echo "usage: ./newTest.sh <name>"
+    exit 1
+fi
+
+target="./tests/$1.html"
+
+if test -f $target; then
+  echo "File exists."
+else
+  cp template.html $target
+fi
+
+cp template.html $target
 code .
-code "./tests/$1.html"
+code $target
